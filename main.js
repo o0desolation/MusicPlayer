6603e0c91b0cce521340edd73569db20b55c0a79
o0desolation <o0desolation@hotmail.com>
2020-04-10 13:23:32 +0800

    base complete

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
-const { app, BrowserWindow } = require("electron");
-require("electron-reload")(__dirname);    //导入electron-reload模块实现热加载
+const { app, BrowserWindow , Menu} = require("electron");
+const path = require('path')
+const url = require('url')
 
 function createWindow() {
 
+  Menu.setApplicationMenu(null)           //windows下去掉菜单栏
+
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 900,
+    center: true,
     webPreferences: {
-      nodeIntegration: true
+      nodeIntegration: true,
     }
   });
- 
-  win.center()
-  win.loadURL("http://localhost:3000/");
+  if(process.env.NODE_ENV === 'development'){
+    win.loadURL("http://localhost:3000/")
+  }
+  else if(process.env.NODE_ENV === 'production'){
+    win.loadURL(url.format({
+      pathname: path.join(__dirname, './build/index.html'),
+      protocol: 'file:',
+      slashes: true
+    }))
+  }
+  else{
+    throw new Error("NODE_ENV is not found")
+  }
+  
+  win.webContents.openDevTools ()
+  
 }
 
 app.whenReady().then(createWindow);
